b99f5957f4a3a85dc5a42d78793f5fa3495e4463
ElleandBeauty <73397470+ElleandBeauty@users.noreply.github.com>
2023-04-27 01:14:58 -0700

    Update preload.py

--- preload.py
+++ preload.py
@@ -1,5 +1,42 @@
 from modules.paths import models_path
 import os
 
+
+import numpy as np
+import pandas as pd
+
+test_set = []
+
+def populate_test_set(begin_idx = 0, end_idx = 104, dataset_fname="TIMED_test_set_filtered_SD14.csv"):
+    df = pd.read_csv(dataset_fname)
+    for idx in range(begin_idx, end_idx):
+        entry = {}
+        entry["old"] = df["old"][idx]
+        entry["new"] = df["new"][idx]
+        entry["positives"] = []
+        entry["negatives"] = []
+        #for gender bias mitigation datasets, validation is in negatives, and test is in positives
+        if "gender" in dataset_fname:
+            entry["negatives"].append({
+                "test": df[f"validation"][idx],
+                "gt": None
+            })
+            for i in range(1, 6):
+                entry["positives"].append({
+                    "test": df[f"ex{i}"][idx],
+                    "gt": None
+                })
+        else:
+            for i in range(1, 6):
+                entry["positives"].append({
+                    "test": df[f"positive{i}"][idx],
+                    "gt": df[f"gt{i}"][idx]
+                })
+                entry["negatives"].append({
+                    "test": df[f"negative{i}"][idx],
+                    "gt": df[f"gn{i}"][idx]
+                })
+                
+                """
 def preload(parser):
-    parser.add_argument("--deltas-dir", type=str, help="finetuned delta weights directory", default=os.path.join(models_path, 'deltas'))
+    parser.add_argument("--deltas-dir", type=str, help="finetuned delta weights directory", default=os.path.join(models_path, 'deltas'))  """
